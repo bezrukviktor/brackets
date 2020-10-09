8fd76232e968561aec293b1cec170bce044179bb
Viktor Bezruk <without.hands95@gmail.com>
2020-10-10 00:57:49 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pairs = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    pairs.push(bracketsConfig[i].join(''), '');
+  }
+
+  for (let i = str.length; i >= 0; i--) {
+    for (let j = 0; j < pairs.length; j++) {
+      str = str.replace(pairs[j], '');
+    }
+  }
+
+  return str.length > 0 ? false : true;
 }
